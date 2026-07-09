a63653df4a4119cc2d535913cfcb5928a81c688e
Site Factory <factory@nis2-pipeline.local>
2026-07-09 14:47:12 +0200

    Wire/update affiliate router (/go)

--- functions/go/[vendor].js
+++ functions/go/[vendor].js
@@ -78,6 +78,11 @@ const MAP = {
     "url": "https://equalizedigital.com/accessibility-checker/?ref=partnerships@nis2-narzedzia.pl",
     "direct": "https://equalizedigital.com/",
     "subid_param": "subid"
+  },
+  "secureframe": {
+    "url": "",
+    "direct": "https://secureframe.com/",
+    "subid_param": "gsxid"
   }
 };
 const GA = {"mid": "G-4D0Y8YWNF3", "secret": "QfSy4Xq2Ql6kPN8sJlsZ8Q"};
